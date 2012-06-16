c4123eae97345806fbe0f37f4b0e2c904b5f57fd
Jason Lewis <jason.lewis1991@gmail.com>
2012-06-16 16:10:54 +0930

    Updated comment block.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- routes.php
+++ routes.php
@@ -1,30 +1,31 @@
 <?php
 
-/**
- * 
- * Example Basset Route
- * 
- * -----------------------------------------------------------------------
- * 
- * The following are example routes, one CSS and one JS, that you can use
- * as a model for your own routes.
- * 
- * Routes for Basset are very simple, the method is either 'styles' or 
- * 'scripts' depending on what you want to use, the first parameter is the
- * route that will be handled. For example:
- * 
- * Basset::styles('website', function($basset) {});
- * 
- * The above will generate a route for yoursite.com/basset/website.css
- * Note the extension is placed automatically for you. You can change
- * the 'basset' by configuring the 'handles' option in your
- * application/bundles.php file.
- * 
- * Please refer to the README.md for further help.
- */
+/*
+|--------------------------------------------------------------------------
+| Basset Routes
+|--------------------------------------------------------------------------
+|
+| Registering routes with Basset is a piece of cake. Simply tell Basset
+| what you would like to use (scripts or styles) and begin registering your
+| assets.
+|
+| Let's register styles that respond to http://localhost/basset/website.css
+|
+| 		Basset::styles('website', function($basset)
+|		{
+|			$basset->add('theme', 'theme.css');
+|		});
+|
+| The extension and the bundles handler is added automatically for you, all
+| you need to do is supply a name.
+|
+| If you'd like to register scripts, simply swap 'styles' with 'scripts'
+| and begin adding your assets.
+|
+*/
 Basset::styles('example', function($basset)
 {
-	$basset->add('website', 'website.css');
+	$basset->add('theme', 'theme.css');
 });
 
 Basset::scripts('example', function($basset)
