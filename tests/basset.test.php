9d3ecf248a97f97cda830ee1e13307d7a5883cc0
Jason Lewis <jason.lewis1991@gmail.com>
2012-08-11 20:28:35 +0930

    Added some test comments.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- tests/basset.test.php
+++ tests/basset.test.php
@@ -2,6 +2,12 @@
 
 class BassetTest extends PHPUnit_Framework_TestCase {
 
+	/**
+	 * Starts basset and creates the mock directory. Not ideal but I'm too lazy to try and
+	 * work in a virtual directory system.
+	 * 
+	 * @return void
+	 */
 	public function setUp()
 	{
 		Bundle::start('basset');
@@ -12,6 +18,11 @@ class BassetTest extends PHPUnit_Framework_TestCase {
 		}
 	}
 
+	/**
+	 * If the mock directory exists we'll delete it in the tear down.
+	 * 
+	 * @return void
+	 */
 	public function tearDown()
 	{
 		if(file_exists(__DIR__ . '/mock'))
@@ -20,6 +31,11 @@ class BassetTest extends PHPUnit_Framework_TestCase {
 		}
 	}
 
+	/**
+	 * Tests that a container can be created.
+	 * 
+	 * @return void
+	 */
 	public function testContainersCanBeCreated()
 	{
 		$container = new Basset\Container(null);
@@ -28,6 +44,11 @@ class BassetTest extends PHPUnit_Framework_TestCase {
 		$this->assertInstanceOf('Basset\\Container', $container);
 	}
 
+	/**
+	 * Tests that style routes can be created.
+	 * 
+	 * @return void
+	 */
 	public function testStyleRoutesCanBeCreated()
 	{
 		Basset::styles('mock', function($basset){});
@@ -37,6 +58,11 @@ class BassetTest extends PHPUnit_Framework_TestCase {
 		$this->routesCanBeCreated($route);
 	}
 
+	/**
+	 * Tests that script routes can be created.
+	 * 
+	 * @return void
+	 */
 	public function testScriptRoutesCanBeCreated()
 	{
 		Basset::scripts('mock', function($basset){});
@@ -46,12 +72,22 @@ class BassetTest extends PHPUnit_Framework_TestCase {
 		$this->routesCanBeCreated($route);
 	}
 
+	/**
+	 * Tests that the routes are created within the Laravel routing system.
+	 * 
+	 * @return void
+	 */
 	private function routesCanBeCreated($route)
 	{
 		$this->assertArrayHasKey($route, Basset::$routes);
 		$this->assertInstanceOf('Laravel\\Routing\\Route', Laravel\Routing\Router::route('GET', $route));
 	}
 
+	/**
+	 * Tests that inline containers can be created.
+	 * 
+	 * @return void
+	 */
 	public function testInlineContainerCanBeCreated()
 	{
 		$container = Basset::inline('mock');
@@ -60,6 +96,11 @@ class BassetTest extends PHPUnit_Framework_TestCase {
 		$this->assertArrayHasKey('inline::mock', Basset::$inline);
 	}
 
+	/**
+	 * Tests that assets are compiled.
+	 * 
+	 * @return void
+	 */
 	public function testAssetsCanBeCompiled()
 	{
 		file_put_contents($file = __DIR__ . '/mock/mock.css', $contents = 'body { background-color: #ff0000; }');
@@ -83,6 +124,11 @@ class BassetTest extends PHPUnit_Framework_TestCase {
 		$this->assertTrue(trim($compiled) === $contents);
 	}
 
+	/**
+	 * Tests that a Basset URL can be created.
+	 * 
+	 * @return void
+	 */
 	public function testBassetURLGenerated()
 	{
 		Basset::styles('mock', function($basset){});
@@ -92,6 +138,11 @@ class BassetTest extends PHPUnit_Framework_TestCase {
 		$this->assertTrue(HTML::style($route) === Basset::show('mock.css'));
 	}
 
+	/**
+	 * Tests that assets can be shared.
+	 * 
+	 * @return void
+	 */
 	public function testAssetsCanBeShared()
 	{
 		Basset::share('mock', 'mock.css');
