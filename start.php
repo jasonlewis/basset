b3ac62f907977705c6a03fdf8498852a2614451b
Jason Lewis <jason.lewis1991@gmail.com>
2012-08-11 20:21:15 +0930

    Yet another restructure. Classes is now libraries.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- start.php
+++ start.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
- * Register all the classes that are used by Basset with the autoloader.
+ * Register the core libraries and vendor libraries that Basset uses with the autoloader.
  */
 Autoloader::map(array(
-	'Basset'			  		  => __DIR__ . DS . 'classes' . DS . 'basset.php',
-	'Basset\\Asset'				  => __DIR__ . DS . 'classes' . DS . 'asset.php',
-	'Basset\\Cache'				  => __DIR__ . DS . 'classes' . DS . 'cache.php',
-	'Basset\\Config'			  => __DIR__ . DS . 'classes' . DS . 'config.php',
-	'Basset\\Container'			  => __DIR__ . DS . 'classes' . DS . 'container.php',
-	'Basset\\Vendor\\CSSCompress' => __DIR__ . DS . 'classes' . DS . 'vendor' . DS . 'csscompress.php',
-	'Basset\\Vendor\\JSMin'		  => __DIR__ . DS . 'classes' . DS . 'vendor' . DS . 'jsmin.php',
-	'Basset\\Vendor\\URIRewriter' => __DIR__ . DS . 'classes' . DS . 'vendor' . DS . 'urirewriter.php',
-	'Basset\\Vendor\\lessc'		  => __DIR__ . DS . 'classes' . DS . 'vendor' . DS . 'less.php'
+	'Basset'			  		  => __DIR__ . DS . 'libraries' . DS . 'core' . DS . 'basset.php',
+	'Basset\\Asset'				  => __DIR__ . DS . 'libraries' . DS . 'core' . DS . 'asset.php',
+	'Basset\\Cache'				  => __DIR__ . DS . 'libraries' . DS . 'core' . DS . 'cache.php',
+	'Basset\\Config'			  => __DIR__ . DS . 'libraries' . DS . 'core' . DS . 'config.php',
+	'Basset\\Container'			  => __DIR__ . DS . 'libraries' . DS . 'core' . DS . 'container.php',
+	'Basset\\Vendor\\CSSCompress' => __DIR__ . DS . 'libraries' . DS . 'vendor' . DS . 'csscompress.php',
+	'Basset\\Vendor\\JSMin'		  => __DIR__ . DS . 'libraries' . DS . 'vendor' . DS . 'jsmin.php',
+	'Basset\\Vendor\\URIRewriter' => __DIR__ . DS . 'libraries' . DS . 'vendor' . DS . 'urirewriter.php',
+	'Basset\\Vendor\\lessc'		  => __DIR__ . DS . 'libraries' . DS . 'vendor' . DS . 'less.php'
 ));
 
 if(starts_with(URI::current(), Bundle::option('basset', 'handles')))
