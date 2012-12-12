21d977e015775e14f1152bd0d852fa7f90d3e70d
Jason Lewis <jason.lewis1991@gmail.com>
2012-12-12 18:41:59 +1100

    Added the version of Basset.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ tasks/version.php
@@ -0,0 +1,15 @@
+<?php
+
+class Basset_Version_Task {
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		echo 'Basset version '.BASSET_VERSION;
+	}
+
+}
