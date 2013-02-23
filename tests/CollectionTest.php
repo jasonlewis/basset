efd8315a76ca4d04b092f01b4bf9260afadfb289
Jason Lewis <jason.lewis1991@gmail.com>
2013-02-23 17:15:59 +1100

    Also test compiling of remote assets.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -168,31 +168,37 @@ class CollectionTest extends PHPUnit_Framework_TestCase {
         $files->shouldReceive('exists')->twice()->with('path/to/js/example.js')->andReturn(true);
         $files->shouldReceive('getRemote')->once()->with('path/to/css/example.css')->andReturn('html { background-color: #fff; }');
         $files->shouldReceive('getRemote')->once()->with('path/to/js/example.js')->andReturn('alert("hello world")');
+        $files->shouldReceive('getRemote')->once()->with('http://foo.com/bar.css')->andReturn('a { text-decoration: none; }');
         $config = $this->getConfig();
         $config->shouldReceive('has')->once()->with('basset::assets.css/example.css')->andReturn(false);
         $config->shouldReceive('has')->once()->with('basset::assets.js/example.js')->andReturn(false);
+        $config->shouldReceive('has')->once()->with('basset::assets.http://foo.com/bar.css')->andReturn(false);
+        $config->shouldReceive('get')->once()->with('basset::compile_remotes', false)->andReturn(true);
         $manager = m::mock('JasonLewis\Basset\AssetManager[getAbsolutePath]', array($files, 'path/to', 'local'));
         $manager->shouldReceive('getAbsolutePath')->with('path/to/css/example.css')->andReturn('path/to/css/example.css');
         $manager->shouldReceive('getAbsolutePath')->with('path/to/js/example.js')->andReturn('path/to/js/example.js');
+        $manager->shouldReceive('getAbsolutePath')->with('http://foo.com/bar.css')->andReturn('http://foo.com/bar.css');
         $instantiatedFilter = m::mock('Assetic\Filter\FilterInterface');
-        $instantiatedFilter->shouldReceive('filterLoad')->once()->andReturn(null);
-        $instantiatedFilter->shouldReceive('filterDump')->once()->andReturnUsing(function($asset)
+        $instantiatedFilter->shouldReceive('filterLoad')->twice()->andReturn(null);
+        $instantiatedFilter->shouldReceive('filterDump')->twice()->andReturnUsing(function($asset)
         {
             $asset->setContent(str_replace('html', 'body', $asset->getContent()));
         });
         $filter = m::mock('JasonLewis\Basset\Filter');
-        $filter->shouldReceive('getFilter')->times(3)->andReturn('BodyFilter');
-        $filter->shouldReceive('getGroupRestriction')->twice()->andReturn('styles');
-        $filter->shouldReceive('getEnvironments')->twice()->andReturn(array());
-        $filter->shouldReceive('instantiate')->once()->andReturn($instantiatedFilter);
+        $filter->shouldReceive('getFilter')->times(4)->andReturn('BodyFilter');
+        $filter->shouldReceive('getGroupRestriction')->times(3)->andReturn('styles');
+        $filter->shouldReceive('getEnvironments')->times(3)->andReturn(array());
+        $filter->shouldReceive('instantiate')->twice()->andReturn($instantiatedFilter);
         $collection = new JasonLewis\Basset\Collection($files, $config, $manager, 'foo');
         $collection->add('css/example.css');
         $collection->add('js/example.js');
+        $collection->add('http://foo.com/bar.css');
         $collection->apply($filter);
         $compiler = new JasonLewis\Basset\Compiler\StringCompiler($files, $config);
         $compiledCss = $compiler->compileStyles($collection);
         $compiledJs = $compiler->compileScripts($collection);
         $this->assertEquals('body { background-color: #fff; }', $compiledCss['path/to/css/example.css']);
+        $this->assertEquals('a { text-decoration: none; }', $compiledCss['http://foo.com/bar.css']);
         $this->assertEquals('alert("hello world")', $compiledJs['path/to/js/example.js']);
     }
 
