79676770e2b9ee6731bb1f0dd51afaada69ee79c
Jason Lewis <jason.lewis1991@gmail.com>
2013-03-04 20:49:02 +1100

    Test coverage for the output resolver.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ tests/Basset/OutputResolverTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Mockery as m;
+use Basset\Output\Resolver;
+use Basset\Manifest\Repository;
+
+class OutputResolverTest extends PHPUnit_Framework_TestCase {
+
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+
+    public function testResolvingOfFingerprtinedCollection()
+    {
+        $collection = m::mock('Basset\Collection');
+        $collection->shouldReceive('getName')->twice()->andReturn('foo');
+
+        $files = m::mock('Illuminate\Filesystem\Filesystem');
+        $files->shouldReceive('exists')->once()->with('path/to/meta/collections.json')->andReturn(true);
+        $files->shouldReceive('get')->once()->with('path/to/meta/collections.json')->andReturn('{"foo":{"fingerprints":{"styles":"bar"}}}');
+
+        $config = m::mock('Illuminate\Config\Repository');
+        $config->shouldReceive('get')->twice()->with('basset::production', array())->andReturn('local', 'production');
+
+        $repository = new Repository($files, 'path/to/meta');
+
+        $repository->load();
+
+        $resolver = new Resolver($repository, $config, 'production');
+
+        $this->assertNull($resolver->resolveFingerprintedCollection($collection, 'styles'));
+        $this->assertEquals('bar', $resolver->resolveFingerprintedCollection($collection, 'styles'));
+    }
+
+
+}
