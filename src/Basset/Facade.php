6ddfb9cf68698d5911d4a9996f4b624ae5fed0b0
Jason Lewis <jason.lewis1991@gmail.com>
2013-03-11 21:35:27 +1100

    Added facade.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ src/Basset/Facade.php
@@ -0,0 +1,14 @@
+<?php namespace Basset;
+
+use Illuminate\Support\Facades\Facade as IlluminateFacade;
+
+class Facade extends IlluminateFacade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'basset'; }
+
+}
