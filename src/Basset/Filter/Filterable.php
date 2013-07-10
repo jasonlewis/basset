c3aa96bc9b252f97516b7a4da8612b45e02a3bd1
Maxime Fabre <ehtnam6@gmail.com>
2013-07-10 15:19:02 +0200

    Fix #155 - All assets shared the same filter

--- src/Basset/Filter/Filterable.php
+++ src/Basset/Filter/Filterable.php
@@ -49,7 +49,7 @@ abstract class Filterable {
             return $this->applyFromArray($filter);
         }
 
-        $filter = $this->factory->get('filter')->make($filter)->setResource($this);
+        $filter = clone $this->factory->get('filter')->make($filter)->setResource($this);
 
         is_callable($callback) and call_user_func($callback, $filter);
 
@@ -66,7 +66,7 @@ abstract class Filterable {
     {
         foreach ($filters as $key => $value)
         {
-            $filter = $this->factory->get('filter')->make(is_callable($value) ? $key : $value)->setResource($this);
+            $filter = clone $this->factory->get('filter')->make(is_callable($value) ? $key : $value)->setResource($this);
 
             is_callable($value) and call_user_func($value, $filter);
 
