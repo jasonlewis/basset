85ba09935f16fc7ebfae9a3f37fa90b49b7f9392
Daniel LaBarge <daniel@bexarcreative.com>
2014-01-20 14:33:44 -0600

    Fix deprecated FileSystem@getRemote()

--- src/Basset/Asset.php
+++ src/Basset/Asset.php
@@ -382,7 +382,7 @@ class Asset extends Filterable {
      */
     public function getContent()
     {
-        return $this->files->getRemote($this->absolutePath);
+        return file_get_contents($this->absolutePath);
     }
 
     /**
